96dcdc989de6fe26981d96323fac3a890654aed1
Ben Johnston <ben.johnston@avcosystems.com>
2017-01-25 08:46:21 +0000

    Calculate the total estimate from our issue convention

--- script/app.js
+++ script/app.js
@@ -4,13 +4,26 @@ var apiUrl = "";
 window.onload = function() {
     loadSettings();
     document.getElementById("projectSelect").onchange = function(e) {
+        allMilestones = [];
         loadMilestones(e.target.value);
     }
 
     document.getElementById("milestoneSelect").onchange = function (e) {
         var sel = document.getElementById("projectSelect");
-        loadIssuesForMilestone(e.target.value, sel.options[sel.selectedIndex].value);
+        var milestone = findMilestone(e.target.value);
+
+        loadIssuesForMilestone(e.target.value, sel.options[sel.selectedIndex].value, milestone);
     }
+}
+
+function findMilestone(id) {
+    for (var i = 0; i < allMilestones.length; i++) {
+        if (allMilestones[i].id == id) {
+            return allMilestones[i];
+        }
+    }
+    
+    return null;
 }
 
 function loadSettings() {
@@ -30,6 +43,8 @@ function loadSettings() {
 
 var currentPage = 0;
 var allProjects = [];
+var allMilestones = [];
+
 function loadAllProjects() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -59,10 +74,14 @@ function loadProjectsComplete() {
         return a.name_with_namespace.localeCompare(b.name_with_namespace);
     });
 
+    var projSelect = document.getElementById("projectSelect");
     for (var i = 0; i < allProjects.length; i++) {
         var opt = newOption(allProjects[i].name_with_namespace, allProjects[i].id);
-        document.getElementById("projectSelect").appendChild(opt);
+        projSelect.appendChild(opt);
     }
+
+    projSelect.value = 46; // for debugging select pl2
+    loadMilestones(46);
 }
 
 function loadMilestones(id) {
@@ -71,9 +90,12 @@ function loadMilestones(id) {
     xhttp.onreadystatechange = function() {
         if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
             var milestones = JSON.parse(this.responseText);
+            allMilestones.push.apply(allMilestones, milestones);
+            var milestoneSelect = document.getElementById("milestoneSelect");
+
             for (var i = 0; i < milestones.length; i++) {
                 var opt = newOption(milestones[i].title, milestones[i].id);
-                document.getElementById("milestoneSelect").appendChild(opt);
+                milestoneSelect.appendChild(opt);
             }
         }
     };
@@ -90,13 +112,31 @@ function newOption(text, value) {
     return option;
 }
 
-function loadIssuesForMilestone(milestoneId, projectId) {
+function loadIssuesForMilestone(milestoneId, projectId, milestone) {
     console.log("Fetching issues for milestone " + milestoneId + " and project " + projectId);
+    console.log("Milestone info: " + milestone);
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
             var issues = JSON.parse(this.responseText);
             console.log("Received " + issues.length + " issues");
+
+            var totalEstimate = 0;
+            var days = 10;
+            issues.forEach(function(item) {
+                var match = item.title.match(/E[0-9]+/i);
+                if (match == null || match == undefined) {
+                    console.log("Invalid description " + item.title);
+                } else {
+                    var estimate = match[0].substring(1, match[0].length); // get rid of the E                
+                    console.log(item.title);
+                    console.log(estimate);
+                    totalEstimate += parseInt(estimate);
+                }
+            });
+
+            
+            console.log("Total completed in milestone " + milestoneId + "\r\n\t" + totalEstimate + " in " + days + " days at " + totalEstimate / days + " points per day");
         }
     };
 
